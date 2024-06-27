892bae42f5caffb03c98185d2ed71f49108af891
Juan Ospina <pildo.dev@gmail.com>
2024-06-26 20:54:40 -0500

    react router and boostrap in installed

---
+++ src/components/MyComponent/MyComponent.tsx
@@ -0,0 +1,14 @@
+// src/components/MyComponent/MyComponent.tsx
+
+import React from 'react';
+import './MyComponent.css'; 
+
+const MyComponent: React.FC = () => {
+  return (
+    <div className="my-component">
+      <h1>Hello from MyComponent!</h1>
+    </div>
+  );
+};
+
+export default MyComponent;
---
+++ src/components/auth/login/login.tsx
@@ -0,0 +1,51 @@
+// src/components/Login/Login.tsx
+
+import React, { useState } from 'react';
+import './login.css';
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h2>2Share</h2>
+        <h4>Login</h4>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+        <div className="register">
+          <span>Don't have an account? </span>
+          <a href="/register">Register</a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
